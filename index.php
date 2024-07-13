9ea95a67389dc7f8a7eec77df11d86cd61e0058b
Xavier Lauer <xavier.lauer@bluewire.fr>
2024-07-13 11:11:42 +0200

    Fix router

--- index.php
+++ index.php
@@ -4,11 +4,19 @@ require_once './vendor/autoload.php';
 
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
-use App\controllers\HomeController;
 
 $route = $_SERVER['REQUEST_URI'];
 
 $loader = new FilesystemLoader('src/templates');
 $twig = new Environment($loader);
 
-echo (new HomeController($twig))->index();
+$className = match ($route) {
+    '/' => 'Home',
+    '/post/{id}' => 'Post',
+    '/admin' => 'Admin',
+    default => 'NotFound',
+};
+
+$controllerString = "App\controllers\\".$className.'Controller';
+$controller = new $controllerString($twig);
+echo $controller->index();
--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -9,11 +9,12 @@ class HomeController
     public function __construct(
         public Environment $twig
     ) {
+
     }
 
     public function index(): string
     {
-        return $this->twig->render('../templates/index.html.twig', [
+        return $this->twig->render('index.html.twig', [
             'name' => 'Test',
         ]);
     }
