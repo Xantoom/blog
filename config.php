f34d8e1a977de6703525f2f71916f3160caea079
Xavier Lauer <xantoom@gmail.com>
2024-08-30 00:54:39 +0200

    Router and fix docker

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+$dbParams = [
+    'driver'   => 'pdo_pgsql',
+    'user'     => $_ENV['POSTGRES_USER'],
+    'password' => $_ENV['POSTGRES_PASSWORD'],
+    'dbname'   => $_ENV['POSTGRES_DB'],
+    'host'     => $_ENV['POSTGRES_HOST'],
+];
+
+// Configuration de Doctrine
+$config = ORMSetup::createAttributeMetadataConfiguration(
+    [__DIR__ . '/src/Entity'],
+    true
+);
+
+$connection = DriverManager::getConnection($dbParams, $config);
+
+$entityManager = new EntityManager($connection, $config);
